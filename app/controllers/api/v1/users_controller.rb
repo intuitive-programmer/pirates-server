af0e1a0f65c1ec99481459a6d20ffa69c770f11b
intuitive-programmer <steven@intuitiveprogrammer.com>
2018-12-03 18:14:43 +0000

    Created model and controller for user

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,43 @@
+class Api::V1::UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update, :delete]
+
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  def show
+    render json: @user
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user
+    else
+      render json: {error: 'Unable to create user.'}
+    end
+  end
+
+  def update
+    @user.update(user_params)
+    render json: @user
+  end
+
+  def delete
+    @user.destroy
+    render json: {message: 'The user has been deleted!'}
+  end
+
+private
+
+  def set_user
+    @user = User.find_by(id: params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(
+      :username
+    )
+  end
+end
